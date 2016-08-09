ee118917a4d870c4d86c63afe6b2a45aa8d24651
Lucas Chaufournier <lucasch@cs>
2016-08-09 14:36:40 -0400

    updated scripts

--- linux/mptcp/switch_mptcp.sh
+++ linux/mptcp/switch_mptcp.sh
@@ -4,7 +4,9 @@
 if sysctl net.mptcp.mptcp_enabled | grep 1; then
     echo "Turning off mptcp"
     sudo sysctl -w net.mptcp.mptcp_enabled=0
+    sudo sysctl -w net.ipv4.tcp_congestion_control="reno" 
 else
     echo "Turning on mptcp"
     sudo sysctl -w net.mptcp.mptcp_enabled=1
+    sudo sysctl -w net.ipv4.tcp_congestion_control="lia" 
 fi
